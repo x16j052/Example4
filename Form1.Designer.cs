08a4f5a2853e8483035a3763b3d0c5d66a4f7bce
WIN_NT\x16j052 <x16j052@fjb-ac.jp>
2017-05-10 12:16:35 +0900

    no message

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Example4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxNum = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblAns = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxNum
+            // 
+            this.tbxNum.Location = new System.Drawing.Point(20, 50);
+            this.tbxNum.Name = "tbxNum";
+            this.tbxNum.Size = new System.Drawing.Size(100, 19);
+            this.tbxNum.TabIndex = 0;
+            this.tbxNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.AutoSize = true;
+            this.btnCalc.Location = new System.Drawing.Point(150, 48);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 1;
+            this.btnCalc.Text = "×10=";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lblAns
+            // 
+            this.lblAns.AutoSize = true;
+            this.lblAns.Location = new System.Drawing.Point(240, 53);
+            this.lblAns.Name = "lblAns";
+            this.lblAns.Size = new System.Drawing.Size(20, 12);
+            this.lblAns.TabIndex = 2;
+            this.lblAns.Text = "???";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.lblAns);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.tbxNum);
+            this.Name = "Form1";
+            this.Text = "変数の利用";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxNum;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblAns;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int ivalue;   //変数宣言
+
+            ivalue = int.Parse(tbxNum.Text);
+            lblAns.Text = String.Format("{0}", ivalue * 10);
+        }
+    }
+}
